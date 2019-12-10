c235191071d62436f459875abdcd7b606054e353
Artur Tarassow <atecon@posteo.de>
2019-12-10 21:47:01 +0100

    Add sh-script for running test-script; correct help file on default delimiter

---
+++ run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=$(dirname $(realpath "$0")) 	# locate folder where this sh-script is located in
+SCRIPT="./tests/run_tests.inp"
+PROJECT="StoreMatrixAsCsv"
+
+cd $DIR
+echo "Switched to ${DIR}"
+
+gretlcli -b -e -q ${SCRIPT}
+
+if [ $? -eq 0 ]
+then
+  echo "Success: All tests passed for '${PROJECT}'."
+  exit 0
+else
+  echo "Failure: Tests not passed for '${PROJECT}'." >&2
+  exit 1
+fi
+
